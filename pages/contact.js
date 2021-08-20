3727c639cf76363a29ad67eaddec421fc2dcec8f
Ujjwal Singh Basnet <ujjwalbas167@gmail.com>
2021-08-20 22:13:07 +0545

    contact page added

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,23 @@
+import { Box, Flex, Heading, Link } from '@chakra-ui/react'
 import React from 'react'
+import Navigation from '../components/Navigation'
 
 function Contact() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Navigation />
+            <Box width='50%' m='0 25%'>
+                <Heading as='h1' fontSize='lg'>
+                    Contact Me
+                </Heading>
+                <Flex>
+                    <Link href='#'>Twitter</Link>
+                    <Link href='#'>Github</Link>
+                    <Link href='#'>LinkedIn</Link>
+                    <Link href='#'>Hashnode</Link>
+                </Flex>
+            </Box>
+        </>
     )
 }
 
